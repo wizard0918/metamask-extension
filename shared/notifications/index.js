aimaniac1989/metamask-review

Show a notification about BSC Swaps if a user is on the BSC Mainnet (#11083)
* Add a notification for BSC Swaps

* Add a new logo for BSC sources, update an image for BSC notification

* Mark a BSC notification as seen when a user clicks on “Start swapping”

* Add conditional rendering of notifications

* Remove divider for the last notification

* Remove a divider for the last notification

* Remove !

* Trigger pipeline

shared/notifications/index.js
@@ -18,6 +18,14 @@ export const UI_NOTIFICATIONS = {
     id: 3,
     date: '2021-03-08',
   },
+  4: {
+    id: 4,
+    date: '2021-05-11',
+    image: {
+      src: 'images/source-logos-bsc.svg',
+      width: '100%',
+    },
+  },
 };
 
 export const getTranslatedUINoficiations = (t, locale) => {
@@ -47,6 +55,15 @@ export const getTranslatedUINoficiations = (t, locale) => {
       actionText: t('notifications3ActionText'),
       date: new Intl.DateTimeFormat(formattedLocale).format(
         new Date(UI_NOTIFICATIONS[3].date),
+      ),
+    },
+    4: {
+      ...UI_NOTIFICATIONS[4],
+      title: t('notifications4Title'),
+      description: t('notifications4Description'),
+      actionText: t('notifications4ActionText'),
+      date: new Intl.DateTimeFormat(locale).format(
+        new Date(UI_NOTIFICATIONS[4].date),
       ),
     },
   };

wizard0918/metamask-extension

shared/notifications/index.js
@@ -18,6 +18,14 @@ export const UI_NOTIFICATIONS = {
     id: 3,
     date: '2021-03-08',
   },
+  4: {
+    id: 4,
+    date: '2021-05-11',
+    image: {
+      src: 'images/source-logos-bsc.svg',
+      width: '100%',
+    },
+  },
   6: {
     id: 6,
     date: '2021-05-26',
@@ -51,6 +59,15 @@ export const getTranslatedUINoficiations = (t, locale) => {
       actionText: t('notifications3ActionText'),
       date: new Intl.DateTimeFormat(formattedLocale).format(
         new Date(UI_NOTIFICATIONS[3].date),
+      ),
+    },
+    4: {
+      ...UI_NOTIFICATIONS[4],
+      title: t('notifications4Title'),
+      description: t('notifications4Description'),
+      actionText: t('notifications4ActionText'),
+      date: new Intl.DateTimeFormat(formattedLocale).format(
+        new Date(UI_NOTIFICATIONS[4].date),
       ),
     },
     6: {